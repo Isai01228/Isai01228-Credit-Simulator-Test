aaa1f034473d6da014dea0dc3bbb54fb8d5159fb
Daniel Isai <daisai1228@hotmail.com>
2022-10-26 11:23:19 -0500

    feat: first commit

---
+++ src/components/formComponent/formComponent.jsx
@@ -0,0 +1,14 @@
+const FormComponent = () => {
+    return(
+        <>
+        <div className="formContain">
+            <form action=""></form>
+        </div>
+        <div className="buttonContain">
+            <button>SOLICITAR MI CREDITO</button>
+        </div>
+        </>
+    )
+}
+
+export default FormComponent
---
+++ src/views/mainView/index.js
@@ -0,0 +1,16 @@
+import '../../styles/App.css'
+import FormComponent from '../../components/formComponent/formComponent.jsx'
+
+const MainView = () => {
+    return(
+        <>
+        <div className="creditContent">
+            <div className='creditView' >
+                <FormComponent/>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default MainView
